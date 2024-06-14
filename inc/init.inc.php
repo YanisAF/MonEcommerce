449d3a5dcd80bab3876df09bff00be836b879332
=YanisAF <yanissf2@gmail.com>
2024-06-14 09:02:23 +0200

    3e commit

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -4,3 +4,10 @@ $mysqli = new mysqli('localhost', 'db_user', '12345', 'monecommerce');
 if ($mysqli->connect_error)
     die('Un problème est survenu lors de la tentative de connexion à la BDD : ' . $mysqli->connect_error);
 // $mysqli->set_charset("utf8");
+
+//--------- SESSION
+session_start();
+ 
+//--------- CHEMIN
+// print_r($_SERVER);
+define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");  //TODO $_SERVER['SERVER_NAME'] . ":"
