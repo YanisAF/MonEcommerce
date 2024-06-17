15e6abcac5ab1f1782131d86686945251ef86cc1
=YanisAF <yanissf2@gmail.com>
2024-06-17 10:24:43 +0200

    modify7

---
+++ connexion.php
@@ -0,0 +1,14 @@
+<?php require_once("./inc/haut.inc.php"); ?>
+
+<form action="" method="post">
+
+    <label for="pseudo">Pseudo</label><br>
+    <input type="text" id="pseudo" name="pseudo"><br><br>
+    <label for="mot_de_passe">Mot de passe</label><br>
+    <input type="password" id="mot_de_passe" name="mot_de_passe"><br><br>
+    <!-- <input type="submit" value=" Se connecter "> -->
+    <button>Se connecter</button>
+
+</form>
+
+<?php require_once("inc/bas.inc.php"); ?>
