1010650db1536114214d46211daf84ac86ee2cfe
=YanisAF <yanissf2@gmail.com>
2024-06-18 21:02:57 +0200

    modify10

--- connexion.php
+++ connexion.php
@@ -1,5 +1,37 @@
-<?php require_once("./inc/haut.inc.php"); ?>
+<?php require_once("./inc/init.inc.php"); 
 
+if(isset($_GET['action']) && $_GET['action'] =="deconnexion") {
+    session_destroy();
+}
+if (internauteEstConnecte()) {
+    header("location:profil.php");
+}
+
+if ($_POST) {
+    $resultat = executeRequete("SELECT * FROM utilisateur WHERE pseudo= '$_POST[pseudo]'");
+    if ($resultat->num_rows != 0){
+        //traitement de connexion
+        $membre = $resultat->fetch_assoc();
+        if (password_verify($_POST['mot_de_passe'], $membre['mot_de_passe'])) {
+            // traitement mdp
+            foreach ($membre as $indice =>$element) {
+                if ($indice != 'mot_de_passe') {
+                    $_SESSION['utilisateur'][$indice] = $element;
+                }
+            }
+            // $contenu .= '<div class="validation">Les identifiants correspondent</div>';
+            header("location:profil.php");
+        } else {
+            $contenu .= '<div class="erreur">Erreur du mot de passe</div>';
+        }
+    } else {
+        $contenu .= '<div class="erreur">Erreur de pseudo</div>';
+    }
+}
+?>
+
+<?php require_once("./inc/haut.inc.php"); ?>
+<?php echo $contenu; ?>
 <form action="" method="post">
 
     <label for="pseudo">Pseudo</label><br>
