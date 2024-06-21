4d85f532645d4989dd77ba26143fddc4105a440b
=YanisAF <yanissf2@gmail.com>
2024-06-21 09:05:49 +0200

    modify13

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -11,3 +11,7 @@ session_start();
 //--------- CHEMIN
 // print_r($_SERVER);
 define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");  //TODO $_SERVER['SERVER_NAME'] . ":"
+
+// Autre inclusions
+require_once("functions.inc.php");
+// debug($mysqli,0);
