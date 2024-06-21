595e9dffae25a08928b9a90137bc704152d36b2b
=YanisAF <yanissf2@gmail.com>
2024-06-21 09:38:50 +0200

    modify16

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,4 +1,7 @@
 <?php
+//* configuration phpdotenv
+require __DIR__ . '/../vendor/autoload.php';
+Dotenv\Dotenv::createUnsafeImmutable(__DIR__ .'/../')->load();
 
 $mysqli = new mysqli('localhost', 'db_user', '12345', 'monecommerce');
 if ($mysqli->connect_error)
@@ -15,3 +18,10 @@ define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");  //TODO $_SERVER
 // Autre inclusions
 require_once("functions.inc.php");
 // debug($mysqli,0);
+
+//* Déclarer une variable d'environement
+putenv('NOM_VARIABLE=Le nom de ma variable d\'environnement avec <span style="font-weight:bold;">putenv(NOM_VARIABLE)</span>');
+//* ou
+$_ENV['NOM_VARIABLE'] = 'Le nom de ma variable d\'environnement avec <span style=font-weight:bold;">$_ENV[NOM_VARIABLE]</span>';
+
+$contenu = "";
