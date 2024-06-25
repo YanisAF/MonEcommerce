6097de00279c0d4117b77828c977c078786b96fb
=YanisAF <yanissf2@gmail.com>
2024-06-25 13:54:14 +0200

    modify17

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require('./inc/init.inc.php');
 
-require('./inc/haut.inc.php');
+require('./inc/haut.inc.php'); ?>
 
-require('./inc/bas.inc.php');
+
+
+<h2>Notre page d'accueil</h2>
+
+<?php require('./inc/bas.inc.php');
+
 ?>
